05219de56bc6baf437e8435f799886b0a78235e2
Dinesh Danda <dinesh.wr150@gmail.com>
2019-07-17 22:20:23 -0500

    FlightDetailsInterface

--- FlightDetailsWatchOS/FlightDetailsWatchOS WatchKit Extension/InterfaceController.swift
+++ FlightDetailsWatchOS/FlightDetailsWatchOS WatchKit Extension/InterfaceController.swift
@@ -12,9 +12,36 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet weak var flightLogoImage: WKInterfaceImage!
+    @IBOutlet weak var routeLabel: WKInterfaceLabel!
+    @IBOutlet weak var flightNameLabel: WKInterfaceLabel!
+    @IBOutlet weak var timeLabel: WKInterfaceLabel!
+    @IBOutlet weak var seatLabel: WKInterfaceLabel!
+    @IBOutlet weak var gateLabel: WKInterfaceLabel!
+    @IBOutlet weak var flightStatusLabel: WKInterfaceLabel!
+    
+    @IBOutlet weak var boardingLabel: WKInterfaceLabel!
+    
+    var flight: Flight? {
+        didSet {
+            guard let flight = flight else { return }
+            flightNameLabel.setText("Flight \(flight.shortNumber)")
+            routeLabel.setText(flight.route)
+            boardingLabel.setText("\(flight.number) Boards")
+            timeLabel.setText(flight.boardsAt)
+            if flight.onSchedule {
+                flightStatusLabel.setText("On Time")
+            } else {
+                flightStatusLabel.setText("Delayed")
+                flightStatusLabel.setTextColor(.red)
+            }
+            gateLabel.setText("Gate \(flight.gate)")
+            seatLabel.setText("Seat \(flight.seat)")
+        }
+    }
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
+          flight = Flight.allFlights().first
         // Configure interface objects here.
     }
     
